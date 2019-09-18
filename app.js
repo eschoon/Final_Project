fe429fdd9690afef208af4398c495879835526f8
eschoon <47728562+eschoon@users.noreply.github.com>
2019-09-18 15:46:42 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var map = L.map('map', {
     center: [40.7128, -74.0060], 
-    zoom: 15
+    zoom: 10
   });
  
   L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
@@ -10,14 +10,30 @@ var map = L.map('map', {
     accessToken: API_KEY
   }).addTo(map);
   
-
+//economic index color switch
+function magColor(economic_need_index) {
+  switch (true){
+    case economic_need_index > 0.9 :
+      return "#FF3F33";
+    case economic_need_index > 0.7:
+      return "#FFA833";
+    case economic_need_index > 0.5:
+      return "#FCFF33";
+    case economic_need_index > 0.3:
+      return "#B2FF33"
+    case economic_need_index > 0.1:
+      return "#33FF52";
+    default:
+      return "#33FFE3"
+  }
+} 
 
   function geojsonMarkerOptions(feature){
 
     return{
-      radius: 4,
-      fillColor: "red",
-      color: "#000000",  //correct
+      radius: 6,
+      fillColor: magColor(feature.properties.economic_need_index), 
+      color: "#000000",  
       weight: 1,
       opacity: 1,
       fillOpacity: 0.8
@@ -26,15 +42,53 @@ var map = L.map('map', {
  
   var customLayer = L.geoJson(null, {
     onEachFeature: function(feature, layer) {
-      layer.bindPopup("<h3>" + feature.properties.school_name +
-      "</h3><hr><p>" + (feature.properties.address) + "</p>"+
-      "</h3><hr><p>" + (feature.properties.school_income_estimate) + "</p>")
+      layer.bindPopup("School Name: " + feature.properties.school_name +
+      "<br>Address: " + (feature.properties.address) + "</br>"+
+      "<br>Economic Need Index: " + (feature.properties.economic_need_index) + "</br>")
       },
       pointToLayer: function (feature, latlng) {
         return L.circleMarker(latlng, geojsonMarkerOptions(feature)
         );
       }
   });
+
+ //create legend
+ var legend  = L.control({
+  position: "bottomright"
+})
+
+
+// add details to legend
+legend.onAdd = function(){
+  // const title = '<h3>Economic Need Index:</h3>';
+  var div = L.DomUtil.create("div", "info legend");
+  var grades = [0, .1, .3, .5, .7, .9, 1];
+  var colors = [
+  "#98ee00",
+  "#d4ee00",
+  "#eecc00",
+  "#ee9c00",
+  "ea822cc",
+  "blue",
+  "ea2c2c"
+];
+  // for (var i = 0; i < grades.length; i++) {
+  //   div.innerHTML +=
+  //    "<i style='background:"  +  colors[i] + "'></i> " + 
+  //    grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + "<br>" : "+");
+  //   }
+  //   return div;
+  // };
+  for (var i = 0; i < grades.length; i++) {
+    div.innerHTML += '<i style="background: ' + colors + '"></i> ' + grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+    return div;
+  };
+
+legend.addTo(map);
+
+
+  
   var runLayer = omnivore.csv('school file.csv', null, customLayer)
       .on('ready', function() {
           
